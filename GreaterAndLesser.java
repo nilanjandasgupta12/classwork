9e68809a00333a7c2d4c7cc888db5e04b0580037
nilanjandasgupta12 <121098696+nilanjandasgupta12@users.noreply.github.com>
2022-12-20 17:39:38 +0530

    Add files via upload

---
+++ GreaterAndLesser.java
@@ -0,0 +1,13 @@
+public class GreaterAndLesser
+{
+public static void main(String[] args)
+{
+int num1=19,num2=28;
+if(num1>num2)
+{
+System.out.println(num1+" is greater");
+}
+else
+System.out.println(num2+" is greater");
+}
+}
